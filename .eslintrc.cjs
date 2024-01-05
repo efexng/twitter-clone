738783916c4b838553044348ef95425fbd2d529f
efexng <efeapoki2020@gmail.com>
2024-01-05 03:38:48 +0000

    changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      // TODO: Remove if/when this gets fixed
+      rules: {
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/restrict-plus-operands": "off",
+      },
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
-    // Ignoring the errors mentioned in your logs:
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars": "off",
-    // @ts-ignore
-    "@typescript-eslint/no-unused-vars-experimental": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
 
