6ca3c5024ac6b86aacbe167ff199bf94dededd39
efexng <efeapoki2020@gmail.com>
2024-01-05 03:31:40 +0000

    chnages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,8 @@ const config = {
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
+    "@typescript-eslint/prefer-optional-chain": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
